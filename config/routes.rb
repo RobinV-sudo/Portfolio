3aaeb81863718ba7e3a03ba16db0a84c78343454
robin vorstedt robinvorstedt@outlook.com <robinvorstedt@outlook.com>
2023-11-02 17:00:02 +0900

    update application.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,9 @@ Rails.application.routes.draw do
   # root "articles#index"
 
   root to: "projects#home"
-  get "about", to: "projects#about"
-  get "project", to: "projects#project"
-  get "contact", to: "projects#contact"
+  get "/about", to: "projects#about"
+  get "/project", to: "projects#project"
+  get "/contact", to: "projects#contact"
 
   get "contact/name", to: "contact#new"
   get "contact/email", to: "contact#new"
